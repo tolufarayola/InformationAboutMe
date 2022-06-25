b89f8b75c1adc143fe4819f5111df264281d76a0
tolufarayola <105269763+tolufarayola@users.noreply.github.com>
2022-06-25 05:54:03 +0100

    Add files via upload
    My first JavaScript Task - Basic information about me

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstname = 'Tolulope';
+let lastname = 'Farayola';
+let height = '5.65ft';
+let country = 'Nigeria';
+ console.log(firstname);
+ console.log(lastname);
+ console.log(height);
+ console.log(country);
